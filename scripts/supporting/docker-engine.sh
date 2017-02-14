169363dc83f2beef1c481e764aeb487c7e367e15
Christoph Leygraf <chris@leygraf.de>
2017-02-14 08:00:11 +0100

    Enabling the docker repo changed

--- scripts/supporting/docker-engine.sh
+++ scripts/supporting/docker-engine.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+echo ""
+echo "=============================================================="
+echo "=============================================================="
+echo ""
+echo "$0 started"
+echo ""
 
 # install docker cs
-sudo rpm --import "https://pgp.mit.edu/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e"
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://packages.docker.com/1.12/yum/repo/main/centos/7
+cat >/etc/yum.repos.d/docker.repo <<-EOF
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/7
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
 sudo yum install docker-engine -y
+
 
 # start docker service
 systemctl enable docker.service
@@ -27,3 +40,10 @@ sudo chown root:docker /usr/local/bin/docker-compose
 cat <<EOF | sudo tee -a /etc/hosts > /dev/null
 ${DOCKER1_IP} dtr.local
 EOF
+
+echo ""
+echo "$0 finished"
+echo ""
+echo "=============================================================="
+echo "=============================================================="
+echo ""
